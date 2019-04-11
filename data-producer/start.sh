be683f705b3cf552d65f6cc311248d0239e8783d
Can Elmas <Can.Elmas@monitise.com>
2019-04-11 10:58:09 +0300

    Update data-producer start.sh

--- data-producer/start.sh
+++ data-producer/start.sh
@@ -5,12 +5,12 @@ stop() {
 }
 
 start() {
-    docker run --name=data-producer -d \
+    docker run --name=data-producer -d --restart=always \
         --network lovely-net \
         -e PERIOD_IN_MS=10000 \
-        -e NUM_OF_USERS=1 \
-        -e SESION_PER_USER=1 \
-        -e EVENTS_PER_SESSION=10 \
+        -e NUM_OF_USERS= \
+        -e SESSION_PER_USER=10 \
+        -e EVENTS_PER_SESSION=30 \
         -e TOPICS_USERS=users \
         -e TOPICS_EVENTS=events \
         -e RUN_MODE=0 \
@@ -18,7 +18,7 @@ start() {
         -e NODE_OPTIONS=--max_old_space_size=4096 \
         -e REDIS_HOST=redis \
         -e BROKER=broker:19092 \
-        -e NODE_ENV=development canelmas/connect-data-producer:$1
+        -e NODE_ENV=production canelmas/connect-data-producer:$1
 }
 
 if [ $# -lt 1 ]; then
