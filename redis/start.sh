2214fdeffb313e9305b12a54742f0c5172990da6
Can Elmas <Can.Elmas@monitise.com>
2019-04-18 17:09:54 +0300

    Update redis start.sh

--- redis/start.sh
+++ redis/start.sh
@@ -2,6 +2,10 @@
 
 DATA_VOLUME="/Users/can/Projects/poc-kafka-hadoop-spark/redis/data"
 IMAGE="redis:5.0.3-alpine3.8"
+PORT=6379
+
+HELP='Usage: start <network:required> <reset:optional>'
+
 
 reset() {
     echo "recreating redis data directory..."
@@ -9,24 +13,24 @@ reset() {
     mkdir -p $DATA_VOLUME
 }
 
-start() {     
-    if [ $# -lt 1 ]; then   
-        echo 'Usage: start <network:required> <reset:optional>'
-        exit 0
-    fi
+start() {
 
     NETWORK=$1
 
-    if [ "$2" == "reset" ]
-    then
+    if [ "$2" == "reset" ]; then
         reset
     fi
 
     echo 'removing existing redis container...'
-    docker stop redis && docker rm -f redis            
+    docker stop redis && docker rm -f redis
 
     echo 'starting redis container...'
-    docker run -d --network=$NETWORK --name redis -v $DATA_VOLUME:/data -p 6379:6379 $IMAGE
+    docker run -d --network=$NETWORK --name redis -v $DATA_VOLUME:/data -p $PORT:6379 $IMAGE
 }
+
+if [ $# -lt 1 ]; then
+    echo $HELP
+    exit 0
+fi
 
 start $1 $2
