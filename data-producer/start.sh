b41fbd30fd0921092f28a6234d5c72a865f1bc9f
Can Elmas <Can.Elmas@monitise.com>
2019-04-22 12:24:52 +0300

    Add network parameter to data-producer start script

--- data-producer/start.sh
+++ data-producer/start.sh
@@ -6,27 +6,27 @@ stop() {
 
 start() {
     docker run --name=data-producer -d --restart=always \
-        --network lovely-net \
+        --network $2 \
         -e PERIOD_IN_MS=10000 \
-        -e NUM_OF_USERS= \
-        -e SESSION_PER_USER=10 \
-        -e EVENTS_PER_SESSION=30 \
+        -e NUM_OF_USERS=5 \
+        -e SESSION_PER_USER=2 \
+        -e EVENTS_PER_SESSION=10 \
         -e TOPICS_USERS=users \
         -e TOPICS_EVENTS=events \
         -e RUN_MODE=0 \
-        -e EVENT_SCENARIO=random \
+        -e EVENT_SCENARIO=commerce \
         -e NODE_OPTIONS=--max_old_space_size=4096 \
         -e REDIS_HOST=redis \
         -e REDIS_PORT=6379 \
-        -e BROKER=broker:19092 \
-        -e VERBOSE=false \
+        -e BROKER=broker:9092 \
+        -e VERBOSE=true \
         -e NODE_ENV=production canelmas/connect-data-producer:$1
 }
 
-if [ $# -lt 1 ]; then
-    echo "Usage: start <version:required>"
+if [ $# -lt 2 ]; then
+    echo "Usage: start <version:required> <network:required>"
     exit 1
 else
     stop
-    start $1
+    start $1 $2
 fi
