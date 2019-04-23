578e013ace93fb656ea17ed14f12c5315a06fc8d
Can Elmas <Can.Elmas@monitise.com>
2019-04-24 00:49:11 +0300

    Add kafka connect to kafka template

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -7,24 +7,48 @@ TOPIC_USERS="users"
 
 ZOOKEPER="zookeeper:12181"
 
+KCONNECT_NODES=("localhost:18083" "localhost:28083")
+
 REPL_FACTOR=1
 PARTITIONS=3
 
 # Create topics
 docker-compose --file $YML exec broker kafka-topics --create \
---zookeeper $ZOOKEPER \
---replication-factor $REPL_FACTOR \
---partitions $PARTITIONS \
---topic $TOPIC_EVENTS
+    --zookeeper $ZOOKEPER \
+    --replication-factor $REPL_FACTOR \
+    --partitions $PARTITIONS \
+    --topic $TOPIC_EVENTS
 
 docker-compose --file $YML exec broker kafka-topics --create \
---zookeeper $ZOOKEPER \
---replication-factor $REPL_FACTOR \
---partitions $PARTITIONS \
---topic $TOPIC_USERS
+    --zookeeper $ZOOKEPER \
+    --replication-factor $REPL_FACTOR \
+    --partitions $PARTITIONS \
+    --topic $TOPIC_USERS
+
+# Connect
+for i in "${!KCONNECT_NODES[@]}"; do    
+    curl ${KCONNECT_NODES[$i]}/connectors -XPOST -H 'Content-type: application/json' -H 'Accept: application/json' -d '{
+        "name": "connect-s3-sink-'$i'",
+        "config": {
+            "topics": "events,users",
+            "connector.class": "io.confluent.connect.s3.S3SinkConnector",
+            "tasks.max" : 4,
+            "flush.size": 50000,
+            "s3.region" : "eu-west-1",
+            "s3.bucket.name" : "byob-raw",
+            "topics.dir": "topics",
+            "storage.class" : "io.confluent.connect.s3.storage.S3Storage",
+            "partitioner.class": "io.confluent.connect.storage.partitioner.TimeBasedPartitioner",
+            "partition.duration.ms" : "3600000",
+            "rotate.interval.ms": "-1",
+            "path.format": "YYYY-MM-dd",
+            "locale" : "US",
+            "timezone" : "UTC",
+            "schema.compatibility": "NONE",
+            "format.class" : "io.confluent.connect.s3.format.json.JsonFormat"
+        }
+    }'
+done
 
 # List topics
 docker-compose --file $YML exec broker kafka-topics --list --zookeeper $ZOOKEPER
-
-# Connect
-#docker-compose --file $YML exec broker connect-standalone /root/config/worker-json.properties /root/config/kafka-to-s3-json-gzip.properties
