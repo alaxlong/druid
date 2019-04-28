a6efc52cfbc6d6f00569e3ee5c9572342f169d3e
Can Elmas <Can.Elmas@monitise.com>
2019-04-29 02:06:14 +0300

    Add random duration (per app) between events

--- data-producer/util.js
+++ data-producer/util.js
@@ -1,11 +1,14 @@
-import moment from "moment";
+import moment from 'moment';
+import _ from 'lodash';
+
+const secondsBetweenEvents = _.sample(_.range(2,15))
 
 export function nextEventTime(lastEventTime) {
-  return moment(lastEventTime, "x").add(2, "seconds").format("x")
+  return moment(lastEventTime, "x").add(secondsBetweenEvents, "seconds").format("x")
 }
 
 export function getSessionStopTime(eventsPerSession, sessionStartTime) {
-  return moment(sessionStartTime, "x").add(2 * (eventsPerSession+1), "seconds").format("x")
+  return moment(sessionStartTime, "x").add(secondsBetweenEvents * (eventsPerSession+1), "seconds").format("x")
 }
 
 export function getSessionDuration(sessionStartTime, sessionStopTime) {
