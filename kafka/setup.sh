cb8277067f1afff11ada2e76f0d4f85222eb39c1
Can Elmas <Can.Elmas@monitise.com>
2019-06-09 19:54:54 +0300

    Add prometheus and grafana to local kafka stack

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -7,48 +7,53 @@ TOPIC_USERS="users"
 
 ZOOKEPER="zookeeper:12181"
 
-KCONNECT_NODES=("localhost:18083" "localhost:28083")
+KCONNECT_NODES=("localhost:18083")
 
 REPL_FACTOR=1
-PARTITIONS=3
+PARTITIONS=2
 
 # Create topics
-docker-compose --file $YML exec broker kafka-topics --create \
+docker-compose --file $YML exec broker /opt/kafka/bin/kafka-topics.sh --create \
     --zookeeper $ZOOKEPER \
     --replication-factor $REPL_FACTOR \
     --partitions $PARTITIONS \
     --topic $TOPIC_EVENTS
 
-docker-compose --file $YML exec broker kafka-topics --create \
+docker-compose --file $YML exec broker /opt/kafka/bin/kafka-topics.sh --create \
     --zookeeper $ZOOKEPER \
     --replication-factor $REPL_FACTOR \
     --partitions $PARTITIONS \
     --topic $TOPIC_USERS
+
+# List topics
+docker-compose --file $YML exec broker kafka-topics --list --zookeeper $ZOOKEPER
 
 # Connect
 for i in "${!KCONNECT_NODES[@]}"; do    
     curl ${KCONNECT_NODES[$i]}/connectors -XPOST -H 'Content-type: application/json' -H 'Accept: application/json' -d '{
         "name": "connect-s3-sink-'$i'",
         "config": {
-            "topics": "events,users",
+            "topics": "events",
             "connector.class": "io.confluent.connect.s3.S3SinkConnector",
             "tasks.max" : 4,
-            "flush.size": 50000,
+            "flush.size": 200,
+            "rotate.schedule.interval.ms": "-1",
+            "rotate.interval.ms": "-1",
             "s3.region" : "eu-west-1",
             "s3.bucket.name" : "byob-raw",
-            "topics.dir": "topics",
+            "s3.compression.type": "gzip",
+            "topics.dir": "deneme",
             "storage.class" : "io.confluent.connect.s3.storage.S3Storage",
-            "partitioner.class": "io.confluent.connect.storage.partitioner.TimeBasedPartitioner",
-            "partition.duration.ms" : "3600000",
-            "rotate.interval.ms": "-1",
+            "partitioner.class": "com.canelmas.kafka.connect.FieldAndTimeBasedPartitioner",
+            "partition.duration.ms" : "3600000",            
             "path.format": "YYYY-MM-dd",
             "locale" : "US",
             "timezone" : "UTC",
             "schema.compatibility": "NONE",
-            "format.class" : "io.confluent.connect.s3.format.json.JsonFormat"
+            "format.class" : "io.confluent.connect.s3.format.json.JsonFormat",
+            "timestamp.extractor": "RecordField",
+            "timestamp.field" : "clientCreationDate",
+            "partition.field" : "appId"
         }
     }'
 done
-
-# List topics
-docker-compose --file $YML exec broker kafka-topics --list --zookeeper $ZOOKEPER
--- kafka/start.sh
+++ kafka/start.sh
@@ -8,36 +8,30 @@ help() {
 }
 
 create_dirs() {    
-    rm -rf $1
-    mkdir -p $1/plugins
-    mkdir -p $1/connect
-    mkdir -p $1/aws
+    rm -rf $1    
+    mkdir -p $1/aws    
+    mkdir -p $1/connectors
+    mkdir -p $1/prometheus
 }
 
-copy_files() {
-    cp config/workers/worker-json.properties $1/connect/
-    cp config/sinks/kafka-to-s3-json-gzip.properties $1/connect/
-    cp config/aws/* $1/aws/
-    tar -C $1/plugins/ -zxf $KAFKA_CONNECT_S3_SINK
+copy_files() { 
+    cp -r config/aws/* $1/aws/    
+    cp -r config/connectors/* $1/connectors/
+    cp -r config/prometheus/* $1/prometheus/
 }
 
 clear_volumes() {
-    docker volume rm docker_zk-data
-    docker volume rm docker_zk-logs
-    docker volume rm docker_broker-logs  
-}
-
-create_network() {
-    docker network rm kafka-net
-    docker network create kafka-net
+    docker volume rm zk-data
+    docker volume rm zk-logs
+    docker volume rm broker-logs  
+    docker volume rm data-grafana
 }
 
 reset() {
     echo 'recreating kafka data directories...'    
     create_dirs $1
     copy_files $1
-    clear_volumes
-    create_network
+    clear_volumes    
 }
 
 start() {
